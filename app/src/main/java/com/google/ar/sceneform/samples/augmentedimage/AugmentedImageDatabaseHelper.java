01a830712d232ec334e59ccf10b63c86487daf62
danielsims1410 <dansims140@gmail.com>
2019-10-08 21:40:32 +0100

    Started Database Helper class • Take data from API and store into database

---
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageDatabaseHelper.java
@@ -0,0 +1,6 @@
+package com.google.ar.sceneform.samples.augmentedimage;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AugmentedImageDatabaseHelper extends SQLiteOpenHelper {
+}
