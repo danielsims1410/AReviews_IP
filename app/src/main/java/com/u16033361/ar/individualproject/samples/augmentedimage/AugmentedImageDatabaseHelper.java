b12185e37e8d0cba0cb866a624a151a58a3a50b6
danielsims1410 <dansims140@gmail.com>
2020-04-26 21:27:08 +0100

    • Final Commit

--- app/src/main/java/com/u16033361/ar/individualproject/samples/augmentedimage/AugmentedImageDatabaseHelper.java
+++ app/src/main/java/com/u16033361/ar/individualproject/samples/augmentedimage/AugmentedImageDatabaseHelper.java
@@ -108,7 +108,7 @@ public class AugmentedImageDatabaseHelper extends SQLiteOpenHelper {
 
         this.currentDay = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
         if(run) {
-            if(recreateDatabase() || true) {
+            if(recreateDatabase()) {
                 new RetrieveData().execute(session);
             } else {
                 setImageDatabase(session);
